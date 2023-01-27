06a32955a7427543ab13eac20fa640a31d06706e
Bernardo Santos <2190723@my.ipleiria.pt>
2023-01-27 15:55:06 +0000

    desenha linha entre currentLocation e coordenadas

--- app/src/main/java/com/example/pokeloot_android/vistas/MapaActivity.java
+++ app/src/main/java/com/example/pokeloot_android/vistas/MapaActivity.java
@@ -2,20 +2,37 @@ package com.example.pokeloot_android.vistas;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.loader.content.AsyncTaskLoader;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.pokeloot_android.R;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 public class MapaActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     GoogleMap map;
+    Location currentLocation;
+    FusedLocationProviderClient fusedLocationProviderClient;
+
+    private static final int REQUEST_CODE = 101;
 
     private double latitude, longitude;
 
@@ -27,19 +44,59 @@ public class MapaActivity extends AppCompatActivity implements OnMapReadyCallbac
         latitude = Double.parseDouble(getIntent().getStringExtra("latitude"));
         longitude = Double.parseDouble(getIntent().getStringExtra("longitude"));
 
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapa);
-        mapFragment.getMapAsync(this);
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        fetchLocation();
+    }
+
+    private void fetchLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+            return;
+        }
+        Task<Location> task = fusedLocationProviderClient.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null) {
+                    currentLocation = location;
+                    SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapa);
+                    assert mapFragment != null;
+                    mapFragment.getMapAsync(MapaActivity.this);
+                }
+            }
+        });
     }
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
         map = googleMap;
         LatLng coordenadas = new LatLng(latitude, longitude);
+        LatLng currLocation = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+
         map.addMarker(new MarkerOptions().position(coordenadas).title("Local do Evento"));
+        map.addMarker(new MarkerOptions().position(currLocation).title("Minha Localização"));
+
+        Polyline polyline = map.addPolyline(new PolylineOptions().add(currLocation, coordenadas));
+        polyline.setTag("Precurso");
+
         map.getUiSettings().setZoomControlsEnabled(true);
         map.setMaxZoomPreference(20);
         map.setMinZoomPreference(5);
-        map.moveCamera(CameraUpdateFactory.newLatLng(coordenadas));
+        map.moveCamera(CameraUpdateFactory.newLatLng(currLocation));
         map.animateCamera(CameraUpdateFactory.newLatLngZoom(coordenadas, 10));
+
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    fetchLocation();
+                }
+                break;
+        }
+    }
+
 }
